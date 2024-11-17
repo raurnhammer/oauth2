e5837a90da8e6b1cebcdf348f8adbc858e414ec8
raurnhammer <157801681+raurnhammer@users.noreply.github.com>
2024-11-17 13:39:55 +0100

    feat: extract code from calling URL

--- js/oauth2.js
+++ js/oauth2.js
@@ -1,2 +1,19 @@
-const tokenElement = document.getElementById("token");
-tokenElement.innerHTML = "123";
+function getQueryParam(paramName) {
+    // Get the query string from the current URL
+    const queryString = window.location.search;
+    
+    // Create a URLSearchParams object
+    const urlParams = new URLSearchParams(queryString);
+    
+    // Get the value of the specific parameter
+    return urlParams.get(paramName);
+}
+
+try {
+    const code = getQueryParam("code");
+    const tokenElement = document.getElementById("token");
+    tokenElement.innerHTML = code;
+} catch (error) {
+    
+}
+
